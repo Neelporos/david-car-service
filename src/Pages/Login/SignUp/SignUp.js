ebfdc68cd597f75ff072f02b65d3fecf3f792d9b
Neelporos <sawkotalimahmud@gmail.com>
2022-04-13 02:51:01 -0700

    require auth component done

--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './SignUp.css';
+import auth from '../../../firebase.init';
 
 const SignUp = () => {
 
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
+
     const navigateLogin = () => {
         navigate('/login')
+    }
+
+    if (user) {
+        navigate('/home')
     }
 
     const handleSignup = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
-        const password = event.target.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='signup-container'>
